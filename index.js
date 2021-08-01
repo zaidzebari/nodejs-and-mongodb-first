e759b75a89191750d0d6aff0181107687866d804
Zaid Salah <zaidsalah351@gmail.com>
2021-08-01 22:37:53 +0300

    added indexjs to connect to mongodb

---
+++ index.js
@@ -0,0 +1,12 @@
+var MongoClient = require('mongodb').MongoClient, assert = require('assert');
+
+//connection url here
+var url = 'mongodb://localhost:27017/myprojectdb';
+
+MongoClient.connect(url, function(err, db) {
+    //assert if error happen, or null
+    assert.equal(null, err);
+    console.log('connected to mongodb server'); 
+    //close connection
+    db.close();
+});
