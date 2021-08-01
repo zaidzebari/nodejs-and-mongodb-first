adc0eb5b7f3f1fc3d9f10c7298ef72a904447a5d
Zaid Salah <zaidsalah351@gmail.com>
2021-08-01 22:39:06 +0300

    updated

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var url = 'mongodb://localhost:27017/myprojectdb';
 MongoClient.connect(url, function(err, db) {
     //assert if error happen, or null
     assert.equal(null, err);
+    //we have connection
     console.log('connected to mongodb server'); 
     //close connection
     db.close();
