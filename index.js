01479d9a2d245367778dd8f16de6fdfcfa68d7bc
Zaid Salah <54475819+zaidzebari@users.noreply.github.com>
2021-08-01 23:05:10 +0300

    db is deprecated now it is client

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ var MongoClient = require('mongodb').MongoClient, assert = require('assert');
 //connection url here
 var url = 'mongodb://localhost:27017/myprojectdb';
 
-MongoClient.connect(url, function(err, db) {
+MongoClient.connect(url, function(err, client) {
     //assert if error happen, or null
     assert.equal(null, err);
     //we have connection
     console.log('connected to mongodb server'); 
     //close connection
-    db.close();
+    client.close();
 });
